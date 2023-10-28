da0fd67a58456cb027be4dd65b661634a9e25909
Popsi1 <umeigboobumneme@gmail.com>
2023-10-28 22:20:23 +0100

    first commit

---
+++ currency-exchange-2/src/main/java/com/example/currencyexchange2/CurrencyExchange2Application.java
@@ -0,0 +1,12 @@
+package com.example.currencyexchange2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchange2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchange2Application.class, args);
+    }
+
+}
