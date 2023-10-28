457cc5a6a15281bb4560997acdb4136680edede8
steven <umeigboobumneme@gmail.com>
2023-10-29 00:43:28 +0100

    add new folder

--- currencyConversion/src/main/java/com/example/currencyconversion/CurrencyConversionController.java
+++ currencyConversion/src/main/java/com/example/currencyconversion/CurrencyConversionController.java
@@ -36,7 +36,7 @@ public class CurrencyConversionController {
 
 //        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
         ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity
-                ("http://localhost:8001/currency-exchange/from/{from}/to/{to}",
+                ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                         CurrencyConversion.class, uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
